51b875cf0108a4fd4029c32cdd2b445cf713ffa2
chinmayiba <72912140+chinmayiba@users.noreply.github.com>
2020-10-19 18:50:43 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,16 @@
+class Details{
+	String name;
+	String place;
+	String college;
+	String DOB;
+	String hobbies;
+
+	void ListingDetails(){
+	System.out.println("name:" + " " + name);
+	System.out.println("place:" + " " + place);
+	System.out.println("college:" + " " + college);
+	System.out.println("Date of birth:" + " " + DOB);
+	System.out.println("Hobbies:" + " " + hobbies);
+	}
+}
+ 
---
+++ TeamDetails.java
@@ -0,0 +1,35 @@
+class TeamDetails{
+	public static void main(String[] args){
+	Details Harshitha= new Details();
+	Harshitha.name= "Harshitha";
+	Harshitha.place= "Hassan";
+	Harshitha.college= "AIT";
+	Harshitha.DOB= "09/06/1998";
+	Harshitha.hobbies="Drawing";
+	Harshitha.ListingDetails();
+
+	Details Girija= new Details();
+	Girija.name= "Girija";
+	Girija.place= "uttara karnataka";
+	Girija.college= "BMIT";
+	Girija.DOB= "03/11/1998";
+	Girija.hobbies= "reading";
+	Girija.ListingDetails();
+
+	Details Sameer= new Details();
+	Sameer.name= "Sameer";
+	Sameer.place= "uttara karnataka";
+	Sameer.college= "BMIT";
+	Sameer.DOB= "07/08/1998";
+	Sameer.hobbies= "playing";
+	Sameer.ListingDetails();
+	
+	Details Karthik= new Details();
+	Karthik.name= "Karthik";
+	Karthik.place= "karnataka";
+	Karthik.college= "BGSIT";
+	Karthik.DOB= "10/12/1998";
+	Karthik.hobbies= "playing";
+	Karthik.ListingDetails();
+	}
+}
