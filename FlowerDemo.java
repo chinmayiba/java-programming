54df065b88356d911f9587b9f39d265b35aa997f
chinmayiba <72912140+chinmayiba@users.noreply.github.com>
2020-10-19 18:54:40 +0530

    Add files via upload

---
+++ FlowerDemo.java
@@ -0,0 +1,20 @@
+class FlowerDemo{
+public static void main(String args[]){
+
+Flower rose=new Flower();
+rose.name="rose";
+rose.color="red";
+
+Flower jasmine=new Flower();
+jasmine.name="jasmine";
+jasmine.color="white";
+
+Flower lotus=new Flower();
+lotus.name="lotus";
+lotus.color="pink";
+
+rose.blooming();
+jasmine.blooming();
+lotus.blooming();
+}
+}
